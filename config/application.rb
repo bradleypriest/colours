8e26f2a8811240a71719bbafbb1566422eef492e
Bradley Priest <bradleypriest@gmail.com>
2012-07-30 12:12:36 +0800

    disable view generators

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,10 @@ module Colours
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators.rails({
+      helpers: false,
+      template_engine: nil
+    })
   end
 end
