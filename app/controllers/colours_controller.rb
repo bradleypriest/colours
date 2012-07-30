ef63899c42b9348b76daa191cf2a05588a849ac5
Bradley Priest <bradleypriest@gmail.com>
2012-07-30 13:34:39 +0800

    add Rails controller

--- app/controllers/colours_controller.rb
+++ app/controllers/colours_controller.rb
@@ -1,2 +1,29 @@
 class ColoursController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Colour.scoped
+  end
+
+  def show
+    respond_with Colour.find(params[:id].to_s)
+  end
+
+  def create
+    @colour = Colour.create(params[:colour])
+    @colour.save
+    respond_with @colour
+  end
+
+  def update
+    @colour = Colour.find(params[:id])
+    @colour.update_attributes(params[:colour])
+    respond_with @colour
+  end
+
+  def destroy
+    @colour = Colour.find(params[:id])
+    @colour.destroy
+    respond_with @colour
+  end
 end
