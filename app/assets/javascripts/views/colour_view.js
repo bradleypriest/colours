3de5646beb3f65a41198a65e49903f07f4c8477c
Bradley Priest <bradleypriest@gmail.com>
2012-07-30 13:34:50 +0800

    set up basic views

--- app/assets/javascripts/views/colour_view.js
+++ app/assets/javascripts/views/colour_view.js
@@ -1,3 +1,24 @@
 Colours.ColourView = Ember.View.extend({
-  templateName: 'colour'
+  templateName: 'colour',
+  attributeBindings: ['style'],
+
+  style: function(){
+    if(!this.get('content.value')){ return };
+    return "color:"+this.get('content.value')
+  }.property('content.value'),
+
+  click: function(event){
+    event.preventDefault();
+    colour = this.get('content')
+    colour.set('likesCount', colour.get('likesCount')+1)
+    colour.transaction.commit()
+  },
+
+  deleteRecord: function(event){
+    event.preventDefault()
+    event.stopPropagation()
+    colour = this.get('content')
+    colour.deleteRecord()
+    colour.get('transaction').commit()
+  }
 });
--- app/assets/javascripts/views/colours_view.js
+++ app/assets/javascripts/views/colours_view.js
@@ -1,3 +1,22 @@
 Colours.ColoursView = Ember.View.extend({
-  templateName: 'colours'
+  templateName: 'colours',
+  content: null,
+  isEditing: false,
+  addNew: function(event){
+    event.preventDefault()
+    this.setProperties({
+      'content': Colours.Colour.createRecord(),
+      'isEditing': true
+    })
+  },
+  save: function(event){
+    event.preventDefault()
+    this.get('content.transaction').commit()
+    this.set('isEditing', false)
+  },
+  cancel: function(event){
+    event.preventDefault()
+    this.get('content.transaction').rollback()
+    this.set('isEditing', false)
+  }
 });
